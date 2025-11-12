3c237d6b7a4886a900670fe3ab9d7461daa0a53d
emmanuel225-star <160597165+emmanuel225-star@users.noreply.github.com>
2025-11-12 10:12:07 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const vehicules = [
+    {
+id:1,
+name:"Bmw Séries 3",
+year: 2021,
+mileage:"25000 Km",
+fuel: "Diesel",
+Price: "35000000 fcfa",
+Image: "https://images.unsplash.com/photo-1555215695-3004980ad54e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80"
+
+},
+
+{
+id:2,
+name: "Bugati La Voiture noir",
+year: 2024,
+mileage: "45000 Km",
+fuel: "Diesel",
+Price: "1100000000 fcfa" ,
+Image: "https://blog.vivacar.fr/wp-content/uploads/2025/03/Top-10-des-plus-belles-voitures-du-monde.webp"
+},
+
+
+{
+ id:3,
+ name: "EXP 100 GT | Bentley",
+ year: 2021,
+ mileage: "33000 Km",
+ fuel: "Diesel",
+ Price: "3400000000 fcfa",
+ Image: "https://i.pinimg.com/1200x/95/a1/ac/95a1ac3549dfc4499662cea57a782660.jpg"
+
+
+
+
+
+
+}
+
+
+
+
+
+
+];
